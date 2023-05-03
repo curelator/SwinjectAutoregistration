b00591c4b37a707f29146936d9e99dd348f68724
judithgomlor <judith.gomez.lorenzo@gmail.com>
2023-05-03 12:31:58 +0200

    Removed Curelator under Swinject dependency

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         .target(
             name: "SwinjectAutoregistration",
             dependencies: [
-                "SwinjectCurelator",
+                "Swinject",
             ],
             path: "Sources"),
     ]
